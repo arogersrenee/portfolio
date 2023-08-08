22b48f624ce2964a85ceec0674aae5391b5160f6
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-08-08 17:14:19 -0500

    js - turn code blocks in Modular patterns

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-// Hamberger Menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
+// Nav - Hamberger Menu Open/Close
+const hamburgerMenu = (() => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+    const mobileMenu = () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+    
+    const closeMenu = () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
-
-
-// Footer Copyright Date
-var date = new Date().getFullYear();
-document.getElementById("year").innerHTML = date;
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+})();
 
 
-// Highlight Filter Labels
+// Footer - Copyright Date
+const footerDate = (() => {
+    var date = new Date().getFullYear();
+    document.getElementById("year").innerHTML = date;
+})();
+
+
+// Filter-section - Highlight Labels
+const highlightLabels = (() => {
 const designLabel = document.getElementById("label-design");
 const frontEndLabel = document.getElementById("label-front-end");
 const viewAllLabel = document.getElementById("label-view-all");
@@ -31,24 +36,28 @@ const designRadio = document.getElementById("design");
 const frontEndRadio = document.getElementById("front-end");
 const viewAllRadio = document.getElementById("view-all");
 
+const highlightDesign = () => {
+    designRadio.checked = true;
+    designLabel.classList.add("label-highlight");
+    frontEndLabel.classList.remove("label-highlight");
+    viewAllLabel.classList.remove("view-all-highlight");
+}
+
+const highlightFrontEnd = () => {
+    frontEndRadio.checked = true;
+    frontEndLabel.classList.add("label-highlight");
+    designLabel.classList.remove("label-highlight");
+    viewAllLabel.classList.remove("view-all-highlight");
+}
+
+const highlightViewAll = () => {
+    viewAllRadio.checked = true;
+    viewAllLabel.classList.add("view-all-highlight");
+    frontEndLabel.classList.remove("label-highlight");
+    designLabel.classList.remove("label-highlight");
+}
+
 designLabel.addEventListener('click', highlightDesign);
 frontEndLabel.addEventListener('click', highlightFrontEnd);
 viewAllLabel.addEventListener('click', highlightViewAll);
-
-function highlightDesign() {
-    designLabel.classList.toggle("label-highlight");
-    frontEndLabel.classList.remove("label-highlight");
-    viewAllLabel.classList.remove("view-all-highlight");
-}
-
-function highlightFrontEnd() {
-    frontEndLabel.classList.toggle("label-highlight");
-    designLabel.classList.remove("label-highlight");
-    viewAllLabel.classList.remove("view-all-highlight");
-}
-
-function highlightViewAll() {
-    viewAllLabel.classList.toggle("view-all-highlight");
-    frontEndLabel.classList.remove("label-highlight");
-    designLabel.classList.remove("label-highlight");
-}
+})();
